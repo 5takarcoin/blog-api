56dc6182db74aa034e805fb52fd62277b4f4a757
Tanvir <5takarcoin@gmail.com>
2023-01-26 19:10:06 +0600

    removed multer

--- index.js
+++ index.js
@@ -7,33 +7,17 @@ const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
 const categoriesRoute = require("./routes/categories");
-const multer = require("multer");
 const path = require("path");
 
 app.use(cors());
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "images")));
 
 mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected"))
   .catch((err) => console.log(err));
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded!");
-});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
